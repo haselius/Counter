945c021d7845974b517f04bdedd484d376bee0df
haselius <tima.larshin@gmail.com>
2020-11-11 20:32:38 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/tust/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tust
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tust/counter_bloc.dart';
+
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          body: BlocProvider(
+              builder: (BuildContext context) => CounterBloc(),
+              child : CounterScreen()
+
+          ),
+        )
+    );
+  }
+}
+
+class CounterScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final _counterBloc = BlocProvider.of<CounterBloc>(context);
+
+    return Scaffold(
+      body:  BlocBuilder<CounterBloc, int>(
+            builder: (BuildContext context, int state){
+              return Center(child:
+              Text("Counte Value : $state",),
+              );
+            },
+          ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+    children: <Widget>[
+    FloatingActionButton(
+        child: Text("+"),
+    onPressed: (){
+    _counterBloc.add(CounterEvents.increment);
+    },
+    ),
+
+    FloatingActionButton(
+    child: Text("-"),
+    onPressed: (){
+    _counterBloc.add(CounterEvents.decrement);
+    },
+    ),
+    ],
+    ),
+
+    );
+
+  }
+}
+
